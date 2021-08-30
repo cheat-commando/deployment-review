98c0b02b67b17836b4ca6fbb7b590d64ca7b128f
Carston Work <carston.work@gmail.com>
2021-08-30 09:30:50 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
